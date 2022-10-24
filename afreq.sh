2214d9c76b27e1503e397809260a7d6ddf786ff6
eylles <ed.ylles1997@gmail.com>
2022-10-23 21:12:11 -0600

    add: key=val parser prototype

--- afreq.sh
+++ afreq.sh
@@ -45,8 +45,34 @@ read_file() {
   done < "$1"
 }
 
+keyval_parse() {
+  old_IFS="$IFS"
+  # Setting 'IFS' tells 'read' where to split the string.
+  while IFS='=' read -r key val; do
+  # Skip over lines containing comments.
+  # (Lines starting with '#').
+  [ "${key##\#*}" ] || continue
+
+  # '$key' stores the key.
+  # '$val' stores the value.
+  printf '%s: %s\n' "$key" "$val"
+
+  # Alternatively replacing 'printf' with the following
+  # populates variables called '$key' with the value of '$val'.
+  #
+  # NOTE: I would extend this with a check to ensure 'key' is
+  #       a valid variable name.
+  # export "$key=$val"
+  #
+  # Example with error handling:
+  # export "$key=$val" 2>/dev/null ||
+  #     printf 'warning %s is not a valid variable name\n' "$key"
+  done < "file"
+  IFS="$old_IFS"
+}
+
 set_governor() {
-  for i in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do 
+  for i in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do
     currentSetting=$(read_file "$i")
       [ "$DBGOUT" = 1 ] && printf '%s current governor: %s\n' "$i" "$currentSetting"
     if [ "$currentSetting" != "$1" ]; then
