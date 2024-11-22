7bce450e890d7b79e1579989876e39a17da02181
eylles <ed.ylles1997@gmail.com>
2024-11-21 23:09:47 -0600

    Add make config

---
+++ config.mk
@@ -0,0 +1,13 @@
+# version
+VERSION = 0.1.0
+
+# PREFIX for install
+PREFIX = /usr/local
+
+# sysvinit scripts available
+RAW_SYSV = acpufreq.is
+INIT_LSB = acpufreq.init
+
+# sysvinit script of choice
+SYSV_SCRIPT = $(RAW_SYSV)
+
