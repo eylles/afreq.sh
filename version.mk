f9ae9cdce2aa1a3babd510648fe25b1c93b26afe
eylles <ed.ylles1997@gmail.com>
2026-09-07 22:08:47 -0600

    Use improved versioning
    Now development versions will show the number of commits since tag and the latest commit hash.

--- version.mk
+++ version.mk
@@ -1,2 +1,10 @@
-# version
-VERSION = 0.5.0
+####################
+# afreq versioning #
+####################
+
+# version determined by git
+GITTAG = $(shell git describe --tags 2>/dev/null)
+# last release tag
+VERS = 0.5.0
+# actual version number that will be used
+VERSION = $(if $(GITTAG),$(GITTAG),$(VERS))
